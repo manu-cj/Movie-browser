620cbcb2290a024d8e56e0de1d05cbaf3c39d54c
manu-cj <carenjotje@gmail.com>
2024-07-31 12:29:39 +0200

    push

--- src/components/TrendingCards.jsx
+++ src/components/TrendingCards.jsx
@@ -4,7 +4,8 @@ import { useState } from 'react';
 function TrendingCards({ movies }) {
     let startNumber = 0
     const [start, setStart] = useState(startNumber)
-
+    let indexNumber = 0;
+    const [startIndex, setStartIndex] = useState(indexNumber)
  
     let endNumber = 3
     const [end, setEnd] = useState(endNumber)
@@ -15,35 +16,60 @@ function TrendingCards({ movies }) {
 
 
     const ClickForNext = () => {
-        setStart(prevStart => prevStart + 1);
-        setEnd(prevEnd => prevEnd + 1);
-        console.log(end);
+      setStartIndex(prevStartIndex => prevStartIndex + 1);
+        if (startIndex === 2) {
+          setStartIndex(0);
+        }
+        if (startIndex == 1) {
+          setStart(prevStart => prevStart + 1);
+          setEnd(prevEnd => prevEnd + 1);
+          setStartIndex(1);
+        }
     }
     
     const ClickForPrevious = () => {
-        setStart(prevStart => prevStart - 1);
-        setEnd(prevEnd => prevEnd - 1);
+       
+        if (start !== 0) {
+          setStart(prevStart => prevStart - 1);
+          setEnd(prevEnd => prevEnd - 1);
+          
+        }
+        else {
+          setStartIndex(0);
+        }
     }
+
+    const sectionStyle = {
+      marginLeft: startIndex === 0 ? '125%' : '0',
+      marginRight: limitedMovies.length === 2 ? '100%' : '0'
+  };
     return (
       <>
         
-
-        <section className="trending-section">
-        {start !== 0 && <button onClick={ClickForPrevious}>-</button>}
+        <h2></h2>
+        {startIndex !== 0 && <button onClick={ClickForPrevious}>-</button>}
+        <section className="trending-section"
+        style={sectionStyle}
+         >
+        
           {limitedMovies.map((movie, index) => (
             <div
               key={movie.id}
-              className={index === 1 ? "selected-movie trending-card" : "trending-card"}
+              className={index === startIndex ? "selected-movie trending-card" : "trending-card"}
               style={{
                 backgroundImage: `url(https://image.tmdb.org/t/p/w200${movie.poster_path})`,
               }}
             >
               <h3>{movie.title}</h3>
+              <h1> {startIndex} </h1>
+              <h1> {start} </h1>
             </div>
           ))}
-                  {end < movies.length - 1 && <button onClick={ClickForNext}>+</button>}
-
         </section>
+        { limitedMovies.length !== 2 && <button onClick={ClickForNext}>+</button>}
+
+        
+
 
       </>
     );
