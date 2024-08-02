2f640e0763aeda827361ee479edbbbf58d78c056
manu-cj <carenjotje@gmail.com>
2024-08-02 16:17:55 +0200

    responsive adjustement

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -2,8 +2,7 @@ import PropTypes from 'prop-types';
 
 const MovieCard = ({ movie, index }) => (
   <div className='card'>
-    <div className='picture-card' style={{
-        height: index % 2 === 0 ? '200px' : '150px',
+    <div className={index%2 === 1 ? "little-picture-card" : "picture-card"} style={{
         backgroundImage:  movie.poster_path === null ? "url(https://media.istockphoto.com/id/1055079680/vector/black-linear-photo-camera-like-no-image-available.jpg?s=612x612&w=0&k=20&c=P1DebpeMIAtXj_ZbVsKVvg-duuL0v9DlrOZUvPG6UJk=)" : `url(https://image.tmdb.org/t/p/w200${movie.poster_path})`
         
       }}>
