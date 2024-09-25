d0e63d5306f46ae36f532611d7f01bf818433aed
Manu <carenjotje@gmail.com>
2024-09-25 11:02:12 +0200

    Update helpers.js

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,4 +1,3 @@
-// Fonctions utilitaires pour le formatage, etc.
 export const formatDate = (dateString) => {
     const options = { year: 'numeric', month: 'long', day: 'numeric' };
     return new Date(dateString).toLocaleDateString(undefined, options);
